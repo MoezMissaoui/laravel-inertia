3ac8e147fb31dc03c20314b0169beea9a390db49
Moez Missaoui <moeiz.missaoui@gmail.com>
2024-01-22 21:42:57 +0100

    default layout

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,13 +1,12 @@
 <template>
-        <div class="container mx-auto px-4">
-            <h1 class="text-3xl font-bold underline mb-5">
-                {{ message }}
-            </h1>
-        </div>
+    <div class="container mx-auto px-4">
+        <h1 class="text-3xl font-bold underline mb-5">
+            {{ message }}
+        </h1>
+    </div>
 </template>
 
 <script setup>
-    import  MainLayout  from "../../Layouts/MainLayout.vue";
     // defineProps(['message'])
     defineProps({
         message: {
@@ -15,10 +14,4 @@
             required: true
         }
     })
-</script>
-
-<script>
-    export default {
-        layout: MainLayout,
-    };
 </script>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,22 +1,16 @@
 <template>
-        <div class="container mx-auto px-4">
-            <h1 class="text-3xl font-bold underline mb-5">
-                {{ message }}
-            </h1>
-        </div>
+    <div class="container mx-auto px-4">
+        <h1 class="text-3xl font-bold underline mb-5">
+            {{ message }}
+        </h1>
+    </div>
 </template>
 
 <script setup>
-    import MainLayout from "../../Layouts/MainLayout.vue";
     defineProps({
         message: {
             type: String,
             required: true,
         },
     });
-</script>
-<script>
-    export default {
-        layout: MainLayout,
-    };
 </script>
