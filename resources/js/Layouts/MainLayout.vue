50f3f0e725eb861bbd56010f966440c97dc74728
Moez Missaoui <moeiz.missaoui@gmail.com>
2024-01-22 21:16:31 +0100

    basic reativity in vue && ref

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -4,6 +4,10 @@
         <Link href="/" class="m-2"> Index Page </Link>
         <br>
         <Link href="/show" class="m-2"> Show Page </Link>
+
+        <div class="ml-20 mt-2">
+            Timer: {{ timer }}
+        </div>
     </div>
 
     <slot></slot>
@@ -12,4 +16,8 @@
 
 <script setup>
     import { Link } from "@inertiajs/vue3";
+    import { ref } from "vue";
+
+    const timer = ref(0);
+    // setInterval(() => timer.value++, 1000);
 </script>
