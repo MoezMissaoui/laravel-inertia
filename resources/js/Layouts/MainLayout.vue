23359df3939c0aca50c9abeb2871bcb89c6f69f1
Moez Missaoui <moeiz.missaoui@gmail.com>
2024-01-22 22:01:02 +0100

    linter && jsconfig

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="flex mb-5">
-        <Link href="/" class="m-2"> Index Page </Link>
-        <br />
-        <Link href="/show" class="m-2"> Show Page </Link>
+  <div class="flex mb-5">
+    <Link href="/" class="m-2"> Index Page </Link>
+    <br />
+    <Link href="/show" class="m-2"> Show Page </Link>
 
-        <div class="ml-20 mt-2">Timer: {{ timer }}</div>
-    </div>
+    <div class="ml-20 mt-2">Timer: {{ timer }}</div>
+  </div>
 
-    <slot></slot>
+  <slot />
 </template>
 
 <script setup>
-    import { Link } from "@inertiajs/vue3";
-    import { ref } from "vue";
+import { Link } from '@inertiajs/vue3'
+import { ref } from 'vue'
 
-    const timer = ref(0);
-    // setInterval(() => timer.value++, 1000);
+const timer = ref(0)
+// setInterval(() => timer.value++, 1000);
 </script>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="container mx-auto px-4">
-        <h1 class="text-3xl font-bold underline mb-5">
-            {{ message }}
-        </h1>
-    </div>
+  <div class="container mx-auto px-4">
+    <h1 class="text-3xl font-bold underline mb-5">
+      {{ message }}
+    </h1>
+  </div>
 </template>
 
 <script setup>
-    defineProps({
-        message: {
-            type: String,
-            required: true,
-        },
-    });
+defineProps({
+  message: {
+    type: String,
+    required: true,
+  },
+})
 </script>
